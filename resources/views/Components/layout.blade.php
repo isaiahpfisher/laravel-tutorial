5a8fabc53b9074011cbdf1b6befe15df73c393b2
isaiahpfisher <isaiahpfisher@gmail.com>
2025-01-01 12:23:10 -0600

    basic blade components

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -6,11 +6,16 @@
   <meta name="viewport"
     content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Index Page</title>
+  <title>Home Page</title>
 </head>
 
 <body>
-  <h1>Hello from the Index Page.</h1>
+  <nav>
+    <x-nav-link>Home</x-nav-link>
+    <x-nav-link>About</x-nav-link>
+    <x-nav-link>Contact</x-nav-link>
+  </nav>
+  {{ $slot }}
 </body>
 
 </html>

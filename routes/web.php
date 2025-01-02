dad4f367b78ce064029920540adb2fc097de2c26
isaiahpfisher <isaiahpfisher@gmail.com>
2025-01-02 16:24:54 -0600

    form submission, csrf, request data, fillable vs guarded

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,26 @@ Route::get('/', function () {
 });
 
 Route::get("/jobs", function () {
-  $jobs = Job::with('employer')->simplePaginate(3);
-  return view('jobs', ['jobs' => $jobs]);
+  $jobs = Job::with('employer')->latest()->simplePaginate(3);
+  return view('jobs/index', ['jobs' => $jobs]);
+});
+
+Route::get("/jobs/create", function () {
+  return view("jobs/create");
 });
 
 Route::get("/jobs/{id}", function ($id) {
-  return view("job", ["job" => Job::find($id)]);
+  return view("jobs/show", ["job" => Job::find($id)]);
+});
+
+Route::post("/jobs", function () {
+  Job::create([
+    'title' => request('title'),
+    'salary' => request('salary'),
+    'employer_id' => 1,
+  ]);
+
+  return redirect("/jobs");
 });
 
 Route::get("/contact", function () {
